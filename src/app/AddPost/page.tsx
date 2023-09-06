d228db9535b3fdad834dcb4638b84691adb37558
MohammadAli Saeidi <1saeidima@gmail.com>
2023-09-06 15:47:04 +0330

    chore: remove draft js chore: add tinyMCE for managing rich text editor chore: add react hook form package feat: add rich text editor in AddPostForm component

---
+++ src/app/AddPost/page.tsx
@@ -0,0 +1,10 @@
+import AddPostForm from "@/components/AddPostForm/AddPostForm";
+import React from "react";
+
+export default function page() {
+	return (
+		<div>
+			<AddPostForm />
+		</div>
+	);
+}
---
+++ src/components/AddPostForm/AddPostForm.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
+export default function AddPostForm() {
+	const editorRef = useRef(null);
+	const log = () => {
+		if (editorRef.current) {
+			console.log(editorRef.current.getContent());
+		}
+	};
+	return (
+		<form>
+			<label>
+				Title
+				<input className="input" type="text" />
+			</label>
+			<Editor
+				onInit={(evt, editor) => (editorRef.current = editor)}
+				initialValue="<p>This is the initial content of the editor.</p>"
+				init={{
+					height: 500,
+					menubar: false,
+					plugins: [
+						"advlist autolink lists link image charmap print preview anchor",
+						"searchreplace visualblocks code fullscreen",
+						"insertdatetime media table paste code help wordcount",
+					],
+					toolbar:
+						"undo redo | formatselect | " +
+						"bold italic backcolor | alignleft aligncenter " +
+						"alignright alignjustify | bullist numlist outdent indent | " +
+						"removeformat | help",
+					content_style:
+						"body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+				}}
+			/>
+			<button onClick={log}>Log editor content</button>
+		</form>
+	);
+}
